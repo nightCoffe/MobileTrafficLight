36661f6297eee1665103dd017b98006332cd055f
nightCoffe <f677f27e671e@mail.ru>
2022-12-28 18:52:30 +0300

    added case, counter,

--- app/src/main/java/com/example/trafficlight/MainActivity.java
+++ app/src/main/java/com/example/trafficlight/MainActivity.java
@@ -6,11 +6,14 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.TransactionTooLargeException;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     private LinearLayout bulb_1, bulb_2, bulb_3;
-    private boolean startStop = true;
+    private boolean startStop = false;
+    private Button button_1;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,26 +22,54 @@ public class MainActivity extends AppCompatActivity {
         bulb_1 = findViewById(R.id.bulb_1);
         bulb_2 = findViewById(R.id.bulb_2);
         bulb_3 = findViewById(R.id.bulb_3);
+        button_1 = findViewById(R.id.button4);
     }
 
     public void onClickStart(View view) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (startStop) {
-                    try {
-                        Thread.sleep(3000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
-    }
 
+        if (!startStop) {
+            button_1.setText("Stop");
+            startStop = true;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (startStop) {
+                        counter++;
+                        switch (counter) {
+                            case 1:
+                                bulb_1.setBackgroundColor(getResources().getColor(R.color.green_1));
+                                bulb_2.setBackgroundColor(getResources().getColor(R.color.grey));
+                                bulb_3.setBackgroundColor(getResources().getColor(R.color.grey));
+                                break;
+                            case 2:
+                                bulb_2.setBackgroundColor(getResources().getColor(R.color.yellow_1));
+                                bulb_1.setBackgroundColor(getResources().getColor(R.color.grey));
+                                bulb_3.setBackgroundColor(getResources().getColor(R.color.grey));
+                                break;
+                            case 3:
+                                bulb_3.setBackgroundColor(getResources().getColor(R.color.red_1));
+                                bulb_1.setBackgroundColor(getResources().getColor(R.color.grey));
+                                bulb_2.setBackgroundColor(getResources().getColor(R.color.grey));
+                                counter = 0;
+                                break;
+                        }
+                        }
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+            }).start();
+        } else {
+            startStop = false;
+            button_1.setText("Start");
+        }
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        startStop  = false;
+        startStop = false;
     }
 }
